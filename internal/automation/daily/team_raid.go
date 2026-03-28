dc6f431a3af33f4470600e2db0a3953c59beae9f
xfengyin <55651142+xfengyin@users.noreply.github.com>
2026-03-28 15:27:35 +0800

    feat: 添加小队突袭任务

---
+++ internal/automation/daily/team_raid.go
@@ -0,0 +1,14 @@
+package daily
+
+// TeamRaid 小队突袭
+func TeamRaid() error {
+	// TODO: 实现小队突袭逻辑
+	// 1. 点击小队突袭入口
+	// 2. 等待进入
+	// 3. 选择难度
+	// 4. 开始战斗
+	// 5. 等待完成
+	// 6. 领取奖励
+	// 7. 重复3次
+	return nil
+}
