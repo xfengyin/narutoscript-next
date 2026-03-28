4a47c99ed77c0b1f8eab6a9c203368b91165b584
xfengyin <55651142+xfengyin@users.noreply.github.com>
2026-03-28 15:27:47 +0800

    feat: 添加周常任务实现

---
+++ internal/automation/weekly/tasks.go
@@ -0,0 +1,31 @@
+package weekly
+
+// Practice 修行之路
+func Practice() error {
+	// TODO: 实现修行之路逻辑
+	return nil
+}
+
+// ChaseAkatsuki 追击晓组织
+func ChaseAkatsuki() error {
+	// TODO: 实现追击晓组织逻辑
+	return nil
+}
+
+// Rebel 叛忍来袭
+func Rebel() error {
+	// TODO: 实现叛忍来袭逻辑
+	return nil
+}
+
+// GuildFortress 组织要塞
+func GuildFortress() error {
+	// TODO: 实现组织要塞逻辑
+	return nil
+}
+
+// Battlefield 天地战场
+func Battlefield() error {
+	// TODO: 实现天地战场逻辑
+	return nil
+}
