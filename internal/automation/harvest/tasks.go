64218e9408cbcf868adf09c2a10c3bc396e5b47b
xfengyin <55651142+xfengyin@users.noreply.github.com>
2026-03-28 15:27:45 +0800

    feat: 添加收获任务实现

---
+++ internal/automation/harvest/tasks.go
@@ -0,0 +1,61 @@
+package harvest
+
+// LuckyMoney 招财
+func LuckyMoney() error {
+	// TODO: 实现招财逻辑
+	return nil
+}
+
+// IntelAgency 情报社
+func IntelAgency() error {
+	// TODO: 实现情报社逻辑
+	return nil
+}
+
+// Ranking 排行榜
+func Ranking() error {
+	// TODO: 实现排行榜领取逻辑
+	return nil
+}
+
+// ActivityBox 活跃度宝箱
+func ActivityBox() error {
+	// TODO: 实现活跃度宝箱领取逻辑
+	return nil
+}
+
+// MonthlySign 每月签到
+func MonthlySign() error {
+	// TODO: 实现每月签到逻辑
+	return nil
+}
+
+// NinjaPass 忍法帖
+func NinjaPass() error {
+	// TODO: 实现忍法帖领取逻辑
+	return nil
+}
+
+// Share 每日分享
+func Share() error {
+	// TODO: 实现每日分享逻辑
+	return nil
+}
+
+// Mail 邮件
+func Mail() error {
+	// TODO: 实现邮件领取逻辑
+	return nil
+}
+
+// StaminaGift 赠送体力
+func StaminaGift() error {
+	// TODO: 实现赠送体力逻辑
+	return nil
+}
+
+// Recruit 招募
+func Recruit() error {
+	// TODO: 实现招募逻辑
+	return nil
+}
